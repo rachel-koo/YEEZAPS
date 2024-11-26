b1352c2397813592106d394788e75ca33ae57c85
rachel-koo <rachel.koo@cooper.edu>
2024-11-26 14:54:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-/**
- * @license
- * Copyright 2024 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-function initMap() {
-  const geocoder = new google.maps.Geocoder();
-  const map = document.querySelector('gmp-map').innerMap;
-  const infoWindow = new google.maps.InfoWindow();
+function toggle()
+{
+ object = document.getElementById("table")
+  if( object.className == "visible" )
+  {
+    object.className = "invisible";
+  }
+  else
+  {
+    object.className = "visible";
+  }
 
-  document.getElementById('submit').addEventListener('click', () => {
-    geocodeLatLng(geocoder, map, infoWindow);
-  });
+
 }
